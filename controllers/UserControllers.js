121a8f9e8b0801f0b148621c9ee32249dc68d6f8
Patt0lino <investmentmoises@gmail.com>
2023-01-08 01:52:18 -0300

    adicionando validações

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -4,8 +4,33 @@ const UserModel  = require('../models/UserModel')
 
 
 exports.CreateController = async (req,res)=>{
-
     const {name, password, email, phone} = req.body
+
+    //validações
+
+    if(!name || name==""||name==undefined || name.lenght<=1){
+        res.json({message:"Preencha o nome corretamente!"})
+    }
+
+    if(!password || password==""||password==undefined || password.lenght<=1){
+        res.json({message:"Preencha a senha corretamente!"})
+    }
+
+    if(!email || email==""||email==undefined || email.lenght<=1){
+        res.json({message:"Preencha o email corretamente!"})
+    }
+
+    if(!phone || phone==""||phone==undefined){
+        res.json({message:"Preencha o nome corretamente!"})
+    }
+
+
+    //checar se usuario existe
+    const checkUserExist = await UserModel.findOne({email,email})
+
+    if(checkUserExist){
+        return res.json({message:"O e-mail já pertence a um usuario, utilize outro!"})
+    }
 
     const user = {
         name,
@@ -17,7 +42,6 @@ exports.CreateController = async (req,res)=>{
 
 
     try{
-        
         UserModel.create(user)
         return res.status(200).json({message:`Usuario criado: ${user}`})
 
